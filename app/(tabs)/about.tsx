01268528d872d2d836a8ab8e497d708907fdf754
Licrua <ilay1275@mail.ru>
2025-05-23 15:30:06 +0300

    probka

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,133 @@
-import { Text, View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function AboutScreen() {
+  const obj = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    21: 'twenty-one',
+    22: 'twenty-two',
+    23: 'twenty-three',
+    24: 'twenty-four',
+    25: 'twenty-five',
+    26: 'twenty-six',
+    27: 'twenty-seven',
+    28: 'twenty-eight',
+    29: 'twenty-nine',
+    30: 'thirty',
+    31: 'thirty-one',
+    32: 'thirty-two',
+    33: 'thirty-three',
+    34: 'thirty-four',
+    35: 'thirty-five',
+    36: 'thirty-six',
+    37: 'thirty-seven',
+    38: 'thirty-eight',
+    39: 'thirty-nine',
+    40: 'forty',
+    41: 'forty-one',
+    42: 'forty-two',
+    43: 'forty-three',
+    44: 'forty-four',
+    45: 'forty-five',
+    46: 'forty-six',
+    47: 'forty-seven',
+    48: 'forty-eight',
+    49: 'forty-nine',
+    50: 'fifty',
+    51: 'fifty-one',
+    52: 'fifty-two',
+    53: 'fifty-three',
+    54: 'fifty-four',
+    55: 'fifty-five',
+    56: 'fifty-six',
+    57: 'fifty-seven',
+    58: 'fifty-eight',
+    59: 'fifty-nine',
+    60: 'sixty',
+    61: 'sixty-one',
+    62: 'sixty-two',
+    63: 'sixty-three',
+    64: 'sixty-four',
+    65: 'sixty-five',
+    66: 'sixty-six',
+    67: 'sixty-seven',
+    68: 'sixty-eight',
+    69: 'sixty-nine',
+    70: 'seventy',
+    71: 'seventy-one',
+    72: 'seventy-two',
+    73: 'seventy-three',
+    74: 'seventy-four',
+    75: 'seventy-five',
+    76: 'seventy-six',
+    77: 'seventy-seven',
+    78: 'seventy-eight',
+    79: 'seventy-nine',
+    80: 'eighty',
+    81: 'eighty-one',
+    82: 'eighty-two',
+    83: 'eighty-three',
+    84: 'eighty-four',
+    85: 'eighty-five',
+    86: 'eighty-six',
+    87: 'eighty-seven',
+    88: 'eighty-eight',
+    89: 'eighty-nine',
+    90: 'ninety',
+    91: 'ninety-one',
+    92: 'ninety-two',
+    93: 'ninety-three',
+    94: 'ninety-four',
+    95: 'ninety-five',
+    96: 'ninety-six',
+    97: 'ninety-seven',
+    98: 'ninety-eight',
+    99: 'ninety-nine',
+    100: 'one hundred',
+  };
+  const PlaceholderImage = require('@/assets/images/background-image.png');
   return (
     <View style={styles.container}>
+      <ImageViewer imgSource={PlaceholderImage} />
       <Text style={styles.text}>About screen</Text>
+      <View style={styles.buttonContainer}>
+        <ActivityIndicator />
+        <Button theme="primary" label="Choose the photo" />
+        <Button label="Use this photo" />
+      </View>
+      <FlatList
+        data={Object.entries(obj)}
+        renderItem={({ item }) => (
+          <Text style={{ color: 'white' }}>{item[0]}</Text>
+        )}
+        keyExtractor={(_, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -17,5 +141,24 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+  buttonContainer: {
+    marginHorizontal: 15,
+    marginVertical: 15,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  button: {
+    marginVertical: 15, // Аналог gap между кнопками
+  },
+  textInput: {
+    height: 40,
+    width: '80%',
+    borderColor: '#aaa',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    color: '#fff',
+    marginTop: 10,
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,47 @@
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+import { useState } from 'react';
+
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-import { View, StyleSheet, Text } from 'react-native';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-        <Text style={styles.text}>dasdasdajsdas</Text>
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage || PlaceholderImage}
+        />
       </View>
-      <View>
-        <Button label="LEJAKй" />
-        <Button label="LEJAKй" />
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -24,18 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    marginTop: 20,
-    padding: 10,
-    backgroundColor: '#007AFF',
-    borderRadius: 5,
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
